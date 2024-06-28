1e8d481f06ea24e6c4c8653af677b205332825ea
SaadHussein <sa3dhusseein@gmail.com>
2024-06-28 19:59:34 +0300

    start create a frontend for graduation prject, just login page and join to group

--- supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://opbbjjxfozbjhhjrjdsl.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wYmJqanhmb3piamhoanJqZHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxOTcwMDcsImV4cCI6MjAxMjc3MzAwN30.hkiH6_iHFqoHU2y4VSf1o5XtFvHWVwUmTs4mkUpMWQU";
-const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
